58c715ae88d9b78f3bd7e8580f7847f7ec0b6ef8
Roberto-Larivera <robylariv@gmail.com>
2024-02-09 22:17:46 +0100

    Aggiunti import necessari e costanti per la gestione delle categorie e delle date formattate. Aggiornate le dipendenze nel file pubspec.yaml.

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,6 +1,11 @@
+import 'package:flutter/material.dart';
+
+import 'package:intl/intl.dart';
 import 'package:uuid/uuid.dart';
 
 const uuid = Uuid();
+
+final formatted = DateFormat.yMd();
 
 enum Category {
   food,
@@ -8,6 +13,13 @@ enum Category {
   leisure,
   work,
 }
+
+const categoryIcons = {
+  Category.food: Icons.lunch_dining,
+  Category.travel: Icons.flight_takeoff,
+  Category.leisure: Icons.movie,
+  Category.work: Icons.work,
+};
 
 class Expense {
   Expense({
@@ -23,4 +35,8 @@ class Expense {
   final double amount; // 1,99
   final DateTime date;
   final Category category;
+
+  get formattedDate {
+    return formatted.format(date);
+  }
 }
