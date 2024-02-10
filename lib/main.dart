1395d4da1ed28838dcbe3fd6af34264369a8f6e9
Roberto-Larivera <robylariv@gmail.com>
2024-02-10 13:58:36 +0100

    Aggiornamento del tema scuro e chiaro

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,37 @@ import 'package:expense_tracker/widgets/expenses.dart';
 ///
 /// Variabili globali che iniziano con la k
 ///
-var kColorScheme =
-    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 95, 59, 181));
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 95, 59, 181),
+);
+var kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 5, 99, 125),
+);
 
 void main() {
   runApp(
     MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: kDarkColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kDarkColorScheme.onPrimaryContainer,
+          foregroundColor: kDarkColorScheme.primaryContainer,
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.primaryContainer,
+            foregroundColor: kDarkColorScheme.onPrimaryContainer,
+          ),
+        ),
+      ),
       theme: ThemeData().copyWith(
         colorScheme: kColorScheme,
         appBarTheme: const AppBarTheme().copyWith(
@@ -36,6 +61,7 @@ void main() {
               fontSize: 17,
             )),
       ),
+      // themeMode: ThemeMode.system //.sistem è default,
       home: const Expenses(),
     ),
   );
