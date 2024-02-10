881c4bb36572b1379820507424720d05223df1be
Roberto-Larivera <robylariv@gmail.com>
2024-02-10 12:28:51 +0100

    Aggiungi variabili globali per lo schema dei colori e modifica il tema dell'app

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,22 @@ import 'package:flutter/material.dart';
 
 import 'package:expense_tracker/widgets/expenses.dart';
 
+///
+/// Variabili globali che iniziano con la k
+///
+var kColorScheme =
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 95, 59, 181));
+
 void main() {
   runApp(
-    const MaterialApp(
-      home: Expenses(),
+    MaterialApp(
+      theme: ThemeData().copyWith(
+          colorScheme: kColorScheme,
+          appBarTheme: const AppBarTheme().copyWith(
+            backgroundColor: kColorScheme.onPrimaryContainer,
+            foregroundColor: kColorScheme.primaryContainer,
+          )),
+      home: const Expenses(),
     ),
   );
 }
