7d593f5e8b5441acad4e2da374b0aaebe860141b
Roberto-Larivera <robylariv@gmail.com>
2024-02-10 14:40:26 +0100

    Aggiunto grafico delle spese

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -40,3 +40,28 @@ class Expense {
     return formatted.format(date);
   }
 }
+
+class ExpenseBucket {
+  const ExpenseBucket({
+    required this.category,
+    required this.expenses,
+  });
+
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+      : expenses = allExpenses
+            .where((expense) => expense.category == category)
+            .toList();
+
+  final Category category;
+  final List<Expense> expenses;
+
+  double get totalExpenses {
+    double sum = 0;
+
+    for (var expense in expenses) {
+      sum += expense.amount;
+    }
+
+    return sum;
+  }
+}
